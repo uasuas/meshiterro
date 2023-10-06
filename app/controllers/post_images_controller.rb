220c111031ef0e6c57e2b740234feb0a08258706
usausa <143808163+uasuas@users.noreply.github.com>
2023-10-06 04:40:38 +0000

    comment

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -10,15 +10,22 @@ class PostImagesController < ApplicationController
     redirect_to post_images_path
   end
 
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to '/post_images'
+  end
+
   def index
     @post_images = PostImage.all
   end
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
-private
+  private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
